803699ae369bb7fdff6c5b62c4926f29f2f58f23
cybertcode <cybertcode@gmail.com>
2022-08-08 17:16:36 -0500

    Personalizado login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,94 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<!DOCTYPE html>
+<html lang="es">
 
-        <x-jet-validation-errors class="mb-4" />
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>LOGIN CYBERTCODE WEB</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('../../backend/assets/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('../../backend/assets/vendors/css/vendor.bundle.base.css') }}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('../../backend/assets/css/style.css') }}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{ asset('../../backend/assets/images/favicon.png') }}" />
+</head>
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+<body>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="m-0 row w-100">
+                <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+                    <div class="mx-auto card col-lg-4">
+                        <div class="px-5 py-5 card-body">
+                            <h3 class="mb-3 text-left card-title">Iniciar sesión</h3>
+                            <form method="POST" action="{{ route('login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="email">Correo electrónico *</label>
+                                    <input type="text" class="form-control p_input" name="email" id="email"
+                                        autofocus>
+                                </div>
+                                @error('email')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                                <div class="form-group">
+                                    <label for="password">Contraseña *</label>
+                                    <input type="password" class="form-control p_input" name="password" id="password"
+                                        autocomplete="current-password">
+                                </div>
+                                @error('password')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                                <div class="form-group d-flex align-items-center justify-content-between">
+                                    <div class="form-check">
+                                        <label for="remember_me" class="form-check-label">
+                                            <input type="checkbox" class="form-check-input" id="remember_me"
+                                                name="remember"> Recordarme </label>
+                                    </div>
+                                    <a href="{{ route('password.request') }}" class="forgot-pass">recuperar
+                                        contraseña</a>
+                                </div>
+                                <div class="text-center">
+                                    <button type="submit" class="btn btn-primary btn-block enter-btn">Acceder al
+                                        sistema</button>
+                                </div>
+                                <div class="d-flex">
+                                    <button class="btn btn-facebook me-2 col">
+                                        <i class="mdi mdi-facebook"></i> Facebook </button>
+                                    <button class="btn btn-google col">
+                                        <i class="mdi mdi-google-plus"></i> Google plus </button>
+                                </div>
+                                <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- content-wrapper ends -->
             </div>
-        @endif
+            <!-- row ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="{{ asset('../../backend/assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="{{ asset('../../backend/assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/misc.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/settings.js') }}"></script>
+    <script src="{{ asset('../../backend/assets/js/todolist.js') }}"></script>
+    <!-- endinject -->
+</body>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+</html>
