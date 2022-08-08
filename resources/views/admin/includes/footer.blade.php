bd178345ae513065fef451a8a4685a3230cf54e8
cybertcode <cybertcode@gmail.com>
2022-08-08 16:28:44 -0500

    Incorporamos plantilla admin

---
+++ resources/views/admin/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="d-sm-flex justify-content-center justify-content-sm-between">
+        <span class="text-center text-muted d-block text-sm-left d-sm-inline-block">Copyright ©
+            CYBERTCODE.com 2022</span>
+        <span class="float-none mt-1 text-center float-sm-right d-block mt-sm-0"> DESARROLLADO <a
+                href="https://www.CYBERTCODE.com/bootstrap-admin-template/" target="_blank">CODERO</a> FROM
+            CYBERTCODE.DEV</span>
+    </div>
+</footer>
